4db18a97be3f751152572635ffdfcdf1a3d65142
Sirri Ayongwa <sirribongnwi7@gmail.com>
2024-10-10 09:11:27 +0100

    Form edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,17 @@ function Footer() {
         <p className="powered-by">Powered By:</p>
         <img src="/images/B-brand logo.jpeg" alt="B-Brand Media Company Logo" className="company-logo" />
         <p className="copyright">Copyright © 2024 All rights reserved | Rivers Carnival</p>
+        <div className="social-icons">
+            <a href="https://facebook.com/Rivcomabuja/" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+        </div>
       </div>
     </footer>
   );
